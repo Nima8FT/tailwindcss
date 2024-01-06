294d01db26b801d89b333254fe675187ceb2d818
Nima8FT <nima_8ak@yahoo.com>
2024-01-06 12:51:49 +0330

    create places

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 var myswiper = new Swiper(".myswiper", {
-    slidesPerView: 1,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var places = new Swiper(".places", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  brakpoints: {
+    640: {
+      slidesPerView: 2,
     },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
 });
