0d1a9c1df5ed17d7c8c733fe9d3f564a4e81468f
Nima8FT <nima_8ak@yahoo.com>
2024-01-07 09:48:52 +0330

    create modal menu for form travel

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,44 @@ var places = new Swiper(".places", {
     },
   }
 });
+
+function handleMenu() {
+  return {
+    open_menu: false,
+    go_next: false,
+    slider_tab: false,
+    tooltip: false,
+    step: 100,
+    handleOpen() {
+      this.go_next = true;
+      setTimeout(() => {
+        this.open_menu = true;
+      }, 1000);
+    },
+    handleClose() {
+      this.tooltip = true
+      document.querySelector('.progressbar').style.width = `${this.step}%`
+      let progress = setInterval(() => {
+        this.step -= 1
+        document.querySelector('.progressbar').style.width = `${this.step}%`
+        if (this.step == 0) {
+          this.tooltip = false
+          this.open_menu = false
+          this.go_next = false
+          this.step = 100
+          clearInterval(progress)
+        }
+      }, 20);
+    },
+    cancelMenu() {
+      this.open_menu = false
+      this.go_next = false
+    },
+    ground() {
+      this.slider_tab = true;
+    },
+    air() {
+      this.slider_tab = false;
+    },
+  }
+}
